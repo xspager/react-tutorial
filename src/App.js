a96bf612a59eddc3ce4ee255c56f3b3162df054d
Maggie Smith <ai_9000@protonmail.com>
2020-12-19 20:14:47 -0600

    Actual working build.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react'
 import Table from './Table'
 
 class App extends Component {
-	render() {
-		return (
-			<div className="container">
-				<Table />
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div className="container">
+                <Table />
+            </div>
+        )
+    }
 }
 
 export default App
